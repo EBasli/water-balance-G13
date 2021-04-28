e6907aa3430eaf31efdaad7dba3327369f509409
ClaraZeh <langstrumpf234@gmail.com>
2021-04-28 11:24:12 +0200

    Update 2018_LotkaVolterra_THe.py

--- 2018_LotkaVolterra_THe.py
+++ 2018_LotkaVolterra_THe.py
@@ -1,217 +1,151 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Apr 28 08:15:49 2017
+#!/usr/bin/env python
+# coding: utf-8
 
-@author: theimovaara
-"""
 
-# matplotlib inline
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy as sp
-from scipy import integrate
-import MyTicToc as mt
+import pandas as pd
+#import MyTicToc as mt
+
+# get_ipython().run_line_magic('matplotlib', 'inline')
+
 
 def tic():
-    # Homemade version of matlab tic and toc functions
+    #Homemade version of matlab tic and toc functions
     import time
     global startTime_for_tictoc
     startTime_for_tictoc = time.time()
 
-
 def toc():
     import time
     if 'startTime_for_tictoc' in globals():
-        print("Elapsed time is " + str(time.time() - startTime_for_tictoc)
-              + " seconds.")
+        print ("Elapsed time is " + str(time.time() - startTime_for_tictoc) + " seconds.")
     else:
-        print("Toc: start time not set")
+        print ("Toc: start time not set")
 
 
-# Definition of parameters
+
+# definition of parameters:
+ba = 28355  # base area [m²] 
+ta = 9100  # top area [m²]
+sw = 38  # slope wdith [m]
+wbh = 12  # waste body height [m]
+clh = 1.5  # cover layer height [m]
+w = 281083000  # waste (wet weight) [kg]
+
+# Storage capacity = 1.5  # [mm/cm depth]
+Scap = 1.5 * 0.1 * clh * ta  # [m³]
 a = 1
-b = 0.25
-c = 0.1
-d = 0.01
+fc = 0.6  # crop factor estimation, grassland
+bc = 7.9  # from Benettin, dont know if right
+bwb = 28  # from Benettin, dont know if right 
+ß0 = 0.85  # from Benettin, dont know if right
+
+'temporary assumed values'
+Scmin = 0.1
+Scmax = 0.9
+Swbmin = 0.2
+Swbmax = 0.9
+Semin = 0.9
+Semax = 0.5
+
+M = pd.read_excel('WieringermeerData_Meteo.xlsx') # index_col='datetime'
+# display(M)
+
+pEv = M.pEV         # potential evapotranspiration [m/day] --> use this way: display(pEv['2003-01-03'])
+R = M.rain_station  # inflow [m/day]
+ 
 
 
-# Definition of Rate Equation
-def dYdt(t, Y):
-    """ Return the growth rate of fox and rabbit populations. """
-    return np.array([a*Y[0] - b*Y[0]*Y[1],
-                     -c*Y[1] + d*Y[0]*Y[1]])
+   
+# Definition of rate equations:
+'What values are predefined and what must be taken from the available data?'
+
+def g(R, t):
+    t = int(t)
+    return R[t]
+J = np.vectorize(g)
+
+def f(pEv, S, t):
+    if S[0] < Semin:
+        fs = 0
+    elif Semin <= S[0] <= Semax:
+        fs = (S[0] - Semin) / (Semax - Semin)
+    else: 
+        fs = 1
+    return pEv[t] * fc * fs
+E = np.vectorize(f)
+
+def h(S, t):
+    return a * ((S[0] - Scmin) / (Scmax - Scmin)) ** bc
+Lc = np.vectorize(h)
+
+def dSdt(S, t):
+    return J(R, t) - Lc(S, t) - E(pEv, S, t)
+
+# def dSdt(t, S):
+#    if S[0] < Semin:
+#        fs = 0
+#    elif Semin <= S[0] <= Semax:
+#        fs = (S[0] - Semin) / (Semax - Semin)
+#    else: 
+#        fs = 1
+#    Et = pEv[t] * fc * fs
+#    Lc = a * ((S[0] - Scmin) / (Scmax - Scmin)) ** bc  # Sc[t]
+ #   Lw = a * ((S[1] - Swbmin) / (Swbmax - Swbmin)) ** bwb  # Swb[t]
+#    ß = ß0 * ((S[0] - Scmin) / (Scmax - Scmin))
+#    Qdr = ß * Lc + Lw
+#    J = Jt[t]
+
+ #   return np.array([J - Lc - Et,
+ #                   (1 - ß) * Lc - Lw])
+ #                   # ß * Lc + Lw - Qdr])
+
 
 
 def main():
-    # Definition of output times
-    tOut = np.linspace(0, 100, 200)              # time
+# Definition of output times (0 to 2757 days)
+    tOut = np.linspace(0, 1500, 2757)  
+    # print(tOut)
     nOut = np.shape(tOut)[0]
-
-    # Initial case, 10 rabbits, 5 foxes
-    Y0 = np.array([10, 5])
-    mt.tic()
+    
+    # Initial case: Sc = 0.7, Swb = 0.8
+    S0 = np.array([0.7, 0.8])
+    
+    import scipy.integrate as spint
+    
+    tic()
     t_span = [tOut[0], tOut[-1]]
-    YODE = sp.integrate.solve_ivp(dYdt, t_span, Y0, t_eval=tOut, 
-                                  method='RK45', vectorized=True, 
-                                  rtol=1e-5 )
+    YODE = spint.solve_ivp(dSdt, t_span, S0, t_eval=tOut, vectorized=True,
+                           method='RK45', rtol=1e-5)
     # infodict['message']                     # >>> 'Integration successful.'
-    rODE = YODE.y[0,:]
-    fODE = YODE.y[1,:]
-
-    '''EULER'''
-    # Initialize output vector
-    YEuler = np.zeros([nOut, 2], dtype=float)
-
-    dtMax = 0.1
-    # dtMin = 1e-11
-    t = tOut[0]
-    iiOut = 0
-
-    # Initialize problem
-    mt.tic()
-    Y = Y0
-    # Write initial values to output vector
-    YEuler[iiOut, [0, 1]] = Y
-    while (t < tOut[nOut-1]):
-        # check time steps
-        Rates = dYdt(t, Y)
-        dtRate = -0.7 * Y/(Rates + 1e-18)
-        dtOut = tOut[iiOut+1]-t
-        dtRate = (dtRate <= 0)*dtMax + (dtRate > 0)*dtRate
-        dt = min(min(dtRate), dtOut, dtMax)
-
-        Y = Y + Rates * dt
-        t = t + dt
-
-        # print ("Time to Output is " + str(np.abs(tOut[iiOut+1]-t)) +
-        # " days.")
-
-        if (np.abs(tOut[iiOut+1]-t) < 1e-5):
-            YEuler[iiOut+1, [0, 1]] = Y
-            iiOut += 1
-
-    rEuler, fEuler = YEuler.T
-    mt.toc()
-
-    '''EULER Predictor Corrector'''
-    # Initialize output vector
-    YPC = np.zeros([nOut, 2], dtype=float)
-
-    dtMax = 0.01
-    # dtMin = 1e-11
-    t = tOut[0]
-    iiOut = 0
-
-    # Initialize problem
-    mt.tic()
-    Y = Y0
-    # Write initial values to output vector
-    YPC[iiOut, [0, 1]] = Y
-    while (t < tOut[nOut-1]):
-        # check time steps
-        Rates = dYdt(t, Y)
-        dtRate = -0.7 * Y/(Rates + 1e-18)
-        # We only need to take the positive rates in to account
-        dtRate = (dtRate < 0)*dtMax + (dtRate >= 0)*dtRate
-        dtOut = tOut[iiOut+1]-t
-        dt = min(min(dtRate), dtOut, dtMax)
-
-        # Iteration with predictor corrector
-        converged = False
-        convCrit = 1e-6
-        Yn = Y + Rates * dt
-        while not(converged):
-            Rn = dYdt(t, Yn)
-            Ynn = Y + (Rates + Rn)/2 * dt
-            if max(np.abs(Yn-Ynn)) < convCrit:
-                converged = True
-            else:
-                Yn = Ynn
-
-        Y = Yn
-        t = t + dt
-    # print ("Time to Output is " + str(np.abs(tOut[iiOut+1]-t)) + " days.")
-
-        if (np.abs(tOut[iiOut+1]-t) < 1e-5):
-            YPC[iiOut+1, [0, 1]] = Y
-            iiOut += 1
-
-    rPC, fPC = YPC.T
-    mt.toc()
-
-    '''RungeKutta'''
-    # Initialize output vector
-    YRK = np.zeros([nOut, 2], dtype=float)
-    dtMax = 0.01
-    # dtMin = 1e-11
-    t = tOut[0]
-    iiOut = 0
-    # Initialize problem
-    mt.tic()
-    Y = Y0
-    # Write initial values to output vector
-    YRK[iiOut, [0, 1]] = Y
-    while (t < tOut[nOut-1]):
-        # check time steps
-        R1 = dYdt(t, Y)
-
-        dtRate = -0.7 * Y/(R1 + 1e-18)
-        # We only need to take the positive rates in to account
-        dtRate = (dtRate < 0)*dtMax + (dtRate >= 0)*dtRate
-        dtOut = tOut[iiOut+1]-t
-        dt = min(min(dtRate), dtOut, dtMax)
-
-        k1 = dt*R1
-        k2 = dt*dYdt(t+dt/2, Y+k1/2)
-        k3 = dt*dYdt(t+dt/2, Y+k2/2)
-        k4 = dt*dYdt(t+dt, Y+k3)
-
-        Y = Y+(k1+2*k2+2*k3+k4) / 6
-        t = t + dt
-
-        # print ("Time to Output is " + str(np.abs(tOut[iiOut+1]-t))
-        # + " days.")
-        if (np.abs(tOut[iiOut+1]-t) < 1e-5):
-            YRK[iiOut+1, [0, 1]] = Y
-            iiOut += 1
-
-    rRK, fRK = YRK.T
-    mt.toc()
-
-    # Plot results with matplotlib
+    ScODE = YODE.y[0,:]
+    SwbODE = YODE.y[1,:]
+    SdrODE = YODE.y[2,:]
+    
+    toc()
+    
     plt.figure()
-    plt.plot(tOut, rODE, 'r-', label='RODE')
-    plt.plot(tOut, fODE, 'b-', label='FODE')
-    plt.plot(tOut, rEuler, 'g+', label='REuler')
-    plt.plot(tOut, fEuler, 'm+', label='FEuler')
-    plt.plot(tOut, rPC, 'rx', label='RPC')
-    plt.plot(tOut, fPC, 'bx', label='FPC')
-    plt.plot(tOut, rRK, 'g.', label='RRK')
-    plt.plot(tOut, fRK, 'm.', label='FRK')
-
+    plt.plot(tOut, ScODE, 'r-', label='Cover layer')
+    plt.plot(tOut, SwbODE  , 'b-', label='Waste body')
+    plt.plot(tOut, SdrODE  , 'g-', label='Drainage')
+    
     plt.grid()
     plt.legend(loc='best')
     plt.xlabel('time')
-    plt.ylabel('population')
-    plt.title('Evolution of fox and rabbit populations')
-    # f1.savefig('rabbits_and_foxes_1.png')
-    plt.show()
+    plt.ylabel('Storage')
+    plt.title('Storage rate landfill')
+
+# f1.savefig('rabbits_and_foxes_1.png')
 
     plt.figure()
-    plt.plot(fODE, rODE, 'b-', label='ODE')
-    plt.plot(fEuler, rEuler, 'b+', label='Euler')
-    plt.plot(fPC, rPC, 'r-', label='Predictor Corrector')
-    plt.plot(fRK, rRK, 'g-', label='Runge Kutta')
-
+    plt.plot(SwbODE, ScODE, 'b-', label='ODE')
     plt.grid()
     plt.legend(loc='best')
-    plt.xlabel('Foxes')    
-    plt.ylabel('Rabbits')
-    plt.title('Evolution of fox and rabbit populations')
-    # f2.savefig('rabbits_and_foxes_2.png')
-    plt.show()
-
+    plt.xlabel('waste body storage')
+    plt.ylabel('Coverlayer storage')
+    plt.title('Storage rate landfill')
 
 if __name__ == "__main__":
     main()
