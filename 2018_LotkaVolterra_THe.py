6da3348e8ed26a85500c32500d6c610469941df2
ClaraZeh <langstrumpf234@gmail.com>
2021-04-30 18:30:37 +0200

    Update 2018_LotkaVolterra_THe.py

--- 2018_LotkaVolterra_THe.py
+++ 2018_LotkaVolterra_THe.py
@@ -6,9 +6,6 @@ import matplotlib.pyplot as plt
 import scipy as sp
 import pandas as pd
 import scipy.integrate as spint
-#import MyTicToc as mt
-
-# get_ipython().run_line_magic('matplotlib', 'inline')
 
 
 def tic():
@@ -37,27 +34,27 @@ w = 281083000           # waste (wet weight) [kg]
 p = 0.35                # from Bennetin
 fc = 0.6                # crop factor estimation, grassland
 
-'where does the 0.5 come from - what source?'
+
 Scmin = ta * clh * p * 0.5  
 Scmax = ta * clh * p
 Swbmin = ba * wbh * p * 0.5
 Swbmax = ba * wbh * p 
 
 'value estimations - to be adjusted'
-a = 741                # starting value 0.48 from Benettin
-bc = 0.26                # 7.9 from Bennetin
-bwb = 100                # 28 from Bennetin
-ß0 = 26.5              # 0.85 from Bennetin
-Semin = 0.2 * Scmax     # start 0.1 * Scmax
-Semax = 0.9 * Scmax     # start 0.9 * Scmax
+a = 741                  # starting value 0.48 from Benettin
+bc = 0.26                # 7.9 from Benettin
+bwb = 100                # 28 from Benettin
+ß0 = 26.5                # 0.85 from Benettin
+Semin = 0.2 * Scmax      # start 0.1 * Scmax
+Semax = 0.9 * Scmax      # start 0.9 * Scmax
 
 M = pd.read_excel('WieringermeerData_Meteo.xlsx') # index_col='datetime'
 L = pd.read_excel('WieringermeerData_LeachateProduction.xlsx')
 
-pEv = M.pEV                     # potential evapotranspiration [m/day] --> use this way: display(pEv['2003-01-03'])
+pEv = M.pEV                     # potential evapotranspiration [m/day] 
 J = M.rain_station              # inflow [m/day]
 # headers edited in excel file
-LP = L.Leachate_Production      # Leachate Production [m³/day]
+LP = L.Leachate_Production      # Leachate Production [m³/day] cumulative
 
    
 # Definition of rate equations:
@@ -87,7 +84,6 @@ def dSdt(t, S):
     dSc = J[int(t)] - Lc - Et
     dSwb = (1 - ß) * Lc - Lw
     return np.array([dSc, dSwb])
-                   # ß * Lc + Lw - Qdr])
 
 def value_correction(ScODE, SwbODE):
     if ScODE < Scmin:
@@ -115,9 +111,8 @@ def L_production(Sc_new, Swb_new):
 def main():
 # Definition of output times (0 to 2757 days)
     tOut = np.linspace(0, 6209, 6210)  
-    #nOut = np.shape(tOut)[0]
     
-    # Initial case: Sc = 0.7, Swb = 0.8
+    # Initial case: Sc and Swb
     S0 = np.array([Semin, Semax])
     
     
@@ -133,23 +128,28 @@ def main():
     
     Qdr_day = L_production(Sc_new, Swb_new) # total daily leachate production
     Qdr_tot = np.cumsum(Qdr_day) # total leachate production in time period defined
-    
-    
-    
+     
     toc()
+    
     
     plt.figure()
     plt.plot(LP.index, LP, 'r-', label='Leachate, measured')
     plt.plot(range(len(Qdr_tot)), Qdr_tot  , 'b-', label='Leachate, simulated')
-    #plt.plot(tOut, Qdr_day, 'g-', label='leachate simulated daily')
-    #plt.plot(tOut, Sc_new, 'l-', label='storage cover layer')
-    #plt.plot(tOut, Swb_new, 'p-', label='storage waste body')
-    
     plt.grid()
     plt.legend(loc='best')
     plt.xlabel('Time [days]')
     plt.ylabel('Leachate [m³]')
     plt.title('Cumulative leachate amount over time - starting date 14.06.2012')
+    
+    plt.figure()
+    #plt.plot(range(len(Qdr_tot)), Qdr_day  , 'b-', label='Leachate, simulated daily')
+    plt.plot(range(len(Qdr_tot)), Sc_new / ta , 'g-', label='Cover layer')
+    plt.plot(range(len(Qdr_tot)), Swb_new / ba , 'b-', label='Waste body')
+    plt.grid()
+    plt.legend(loc='best')
+    plt.xlabel('Time [days]')
+    plt.ylabel('Water storage [m]')
+    plt.title('Water storage in cover layer and waste body')
     
 
     plt.figure()
